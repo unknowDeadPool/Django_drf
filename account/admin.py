154ae5380ce7386f0fd49f131918a566ee4d9ba4
unknowDeadPool <arsa.ismail051825@gmail.com>
2023-02-10 00:10:36 +0600

    Django_drf

---
+++ account/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from account.models import MyUser
+
+admin.site.register(MyUser)
---
+++ main/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from main.models import *
+
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    max_num = 10
+    min_num = 1
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageInline, ]
+
+admin.site.register(Category)
